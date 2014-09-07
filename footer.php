aef43f39b09ed6c0d4acb69459390ddc2db11483
Seita <zen@seita.me>
2014-09-07 15:47:54 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 <footer id="footer" role="contentinfo">
 <div class="container">
     二〇一四 • <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a><br>
-<span class="copy-meta">♥ Thank you <a href="http://seita.me/go/do" rel="nofollow" target="_blank">DigitalOcean</a>.</span>
+<span class="copy-meta">♥ Made by <a href="k.chan.gd" rel="nofollow" target="_blank">Kchan</a>.</span>
 
 </div>
 </footer><!-- end #footer -->
-<div style="display:none;"><script src="http://s23.cnzz.com/stat.php?id=5821639&web_id=5821639" language="JavaScript"></script></div>
 <?php $this->footer(); ?>
 </body>
 </html>
